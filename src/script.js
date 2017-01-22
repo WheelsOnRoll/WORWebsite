7a1269a9ac08c63139e09223be0b5c8546060878
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2017-01-22 15:02:51 +0530

    Footer done

--- src/script.js
+++ src/script.js
@@ -3,5 +3,7 @@ function init(){
 		$('.home-text').css({'margin-top': '0', 'opacity': '1'});
 		$('.navblocks').css({'margin-top': '0', 'opacity': '1'});
 		$('.materialboxed').materialbox();
+		$('.slider').slider({indicators: false, transition: 1000, interval: 4000});
+		$('.slider').slider('start');
 	}, 1000);
 }
