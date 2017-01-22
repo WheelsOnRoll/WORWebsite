835db05f55a2bbdea4ddaf6c862cae28ceb7508d
Salman Shah <sbs.191197@gmail.com>
2017-01-22 11:50:23 +0530

    Team Page Done (#10)

--- src/script.js
+++ src/script.js
@@ -2,5 +2,6 @@ function init(){
 	setTimeout(function(){
 		$('.home-text').css({'margin-top': '0', 'opacity': '1'});
 		$('.navblocks').css({'margin-top': '0', 'opacity': '1'});
+		$('.materialboxed').materialbox();
 	}, 1000);
 }
