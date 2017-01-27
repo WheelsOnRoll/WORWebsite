a9da7e5cb4f2efb5228600a3423cdd65fc9b2d28
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2017-01-28 01:48:14 +0530

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ function init(){
 	setTimeout(function(){
 		$('.home-text').css({'margin-top': '0', 'opacity': '1'});
 		$('.navblocks').css({'margin-top': '0', 'opacity': '1'});
+		$('.container').css({'margin-top': '0', 'opacity': '1'});
 		$('.materialboxed').materialbox();
 		$('.slider').slider({indicators: false, transition: 1000, interval: 4000});
 		$('.slider').slider('start');
